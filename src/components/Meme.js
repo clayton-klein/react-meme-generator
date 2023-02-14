cc9f8de8254927fb6097cebac7eef787fc63fd31
clayton-klein <csklein87@hotmail.com>
2023-02-14 14:14:59 -0300

    feat: add dynamic imgs and top/bottom text of them

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,28 +1,73 @@
+import { useState, useEffect } from "react";
 import "../styles/Meme.css";
 
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    img: "./images/meme-img.png",
+  });
+
+  const [memesArray, setMemesArray] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((memeData) => setMemesArray(memeData.data.memes));
+  }, []);
+
+  function changeMemeImg(event) {
+    event.preventDefault();
+
+    let randomNumber = Math.floor(Math.random() * memesArray.length + 1);
+    let randomImg = memesArray[randomNumber].url;
+
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        img: randomImg,
+      };
+    });
+  }
+
+  function handleInputsChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
   return (
-    <section className="memeContainer">
+    <main className="memeContainer">
       <form className="memeForm">
-        <div className="form__TextInputsContainer">
+        <div className="form__textInputsContainer">
           <input
             className="form__topText"
+            name="topText"
+            onChange={handleInputsChange}
+            placeholder="Top text"
             type="text"
-            placeholder="Top text here"
+            value={meme.topText}
           />
           <input
             className="form__bottomText"
+            name="bottomText"
+            onChange={handleInputsChange}
+            placeholder="Bottom text"
             type="text"
-            placeholder="Bottom text here"
+            value={meme.bottomText}
           />
         </div>
-        <input
-          className="form__button"
-          type="button"
-          value="Get a New Meme Image 🖼️"
-        />
+        <button className="form__button" onClick={changeMemeImg}>
+          Get a New Meme Image 🖼️
+        </button>
       </form>
-      <img className="memeContainer__memeImg" src="./images/meme-img.png" alt="" />
-    </section>
+      <div className="memeImgContainer">
+        <img className="memeImgContainer__memeImg" src={meme.img} alt="" />
+        <p className="memeImgContainer__text--top">{meme.topText}</p>
+        <p className="memeImgContainer__text--bottom">{meme.bottomText}</p>
+      </div>
+    </main>
   );
 }
